2ffa0f949a9a22aa524f9112af39f3221b20e938
InfFlps <inflps@proton.me>
2026-02-03 10:48:25 +0800

    Improve motor functions and sensor calibration
    Refactor motor control and sensor management for line tracking. Fixed the bugs

--- line-tracking-robot.ino
+++ line-tracking-robot.ino
@@ -1,79 +1,106 @@
-/**
-  * ?notice Variables with "const" aren't meant to be changed
-  */
+#include <QTRSensors.h>
 
-#define AIN1 13 // left motor
-#define AIN2 14 // left motor
+#define AIN1 13
+#define AIN2 14
+#define BIN1 32
+#define BIN2 33
+#define PWMA 25
+#define PWMB 26
 
-#define BIN1 32 // right motor
-#define BIN2 33 // right motor
+const uint32_t PWM_FREQ = 20000;
+const uint8_t  PWM_RES  = 8;
 
-#define PWMA 25 // left motor
-#define PWMB 26 // right motor
+QTRSensors qtr;
 
-#include "QTRSensors.h" // includes the library that handles the sensors
-QTRSensors qtr; // initializes the library as vaiable qtr
+uint8_t sensorPins[] = {27, 23, 22, 18, 19, 21, 16, 17};
+const uint8_t sensorCount = 8;
+uint16_t sensorValues[sensorCount];
 
-uint8_t sensorPins[] = {
-  27 /*sensor 1*/,
-  23 /*sensor 2*/,
-  22 /*sensor 3*/,
-  18 /*sensor 4*/,
-  19 /*sensor 5*/,
-  21 /*sensor 6*/,
-  16 /*sensor 7*/,
-  17 /*sensor 8*/
-  }; const uint16_t sensorValues[]; const uint8_t sensorCount = 8;
-const uint16_t threshold = 1000;
+void motor(uint8_t leftSpeed, uint8_t rightSpeed) {
+  digitalWrite(AIN1, HIGH);
+  digitalWrite(AIN2, LOW);
+  digitalWrite(BIN1, HIGH);
+  digitalWrite(BIN2, LOW);
 
-/**
-  * void motor(){} Manages the motor
-  * @param left handles the left motor speed
-  * @param right handles the right motor speed
-  */
-void motor(int left, int right) {
-  digitalWrite(AIN1, HIGH); digitalWrite(AIN2, LOW); digitalWrite(BIN1, HIGH); digitalWrite(BIN2, LOW);
-  digitalWrite(PWMA, left); digitalWrite(PWMB, right);
-} void straight() {motor(200, 200);}
-void left() {motor(20, 200);} void right() {motor(200, 20);} void stop() {motor(0, 0);}
-
-void qtrManager(uint8_t &left, uint8_t &middle, uint8_t &right) {
-  qtr.read(sensorValues);
-  uint8_t sensor[8];
-  for(int i = 0; i < 8; i++) {
-    sensor[i] = (sensorValues[i] > threshold) ? 1 : 0;
-  }
-
-  left = sensor[0] | sensor[1];
-  middle = sensor[2] | sensor[3] | sensor[4] | sensor[5];
-  right = sensor[6] | sensor[7];
+  ledcWrite(PWMA, leftSpeed);
+  ledcWrite(PWMB, rightSpeed);
 }
-void setup() {
-  qtr.setTypeRC(); // tells the library that the sensor is RC, not an analog
-  qtr.setSensorPins(sensorPins, sensorCount) // this function automatically handles all timing and calibration
-  for(int i = 0; i < 400; i++) {
-    qtr.calibrate();
-    delay(5);
+
+void moveStart() { for(int i = 0; i <= 255; i++) {motor(i, i); delay(10);}}
+void goStraight() { motor(255, 255); }
+void turnLeft()   { motor(80, 255);  }
+void turnRight()  { motor(255, 80);  }
+void stopMotor()  { motor(0, 0);     }
+
+void qtrManager(uint8_t &l, uint8_t &m, uint8_t &r) {
+  qtr.read(sensorValues);
+
+  uint8_t s[8];
+
+  for (int i = 0; i < 8; i++) {
+    uint16_t minV = qtr.calibrationOn.minimum[i];
+    uint16_t maxV = qtr.calibrationOn.maximum[i];
+    uint16_t th   = (minV + maxV) / 2;
+
+    s[i] = (sensorValues[i] > th) ? 1 : 0;
   }
+
+  l = s[0] | s[1];
+  m = s[2] | s[3] | s[4] | s[5];
+  r = s[6] | s[7];
+}
+
+void setup() {
+  Serial.begin(115200);
+
   pinMode(AIN1, OUTPUT);
   pinMode(AIN2, OUTPUT);
   pinMode(BIN1, OUTPUT);
-  pinMode(BIN1, OUTPUT);
-  pinMode(PWMA, OUTPUT);
-  pinMode(PWMB, OUTPUT);
-  stop(); // stops the motor at first initialization (for debugging porposes)
+  pinMode(BIN2, OUTPUT);
+
+  ledcAttach(PWMA, PWM_FREQ, PWM_RES);
+  ledcAttach(PWMB, PWM_FREQ, PWM_RES);
+
+  qtr.setTypeRC();
+  qtr.setSensorPins(sensorPins, sensorCount);
+
+  for (int i = 0; i < 400; i++) {
+    qtr.calibrate();
+    delay(5);
+  }
+
+  stopMotor();
 }
 
 void loop() {
-  uint8_t left, middle, right;
-  qtrManager(left, middle, right);
-  if(left == 0 && middle == 1 && right == 0) {
-    straight();
-  } else if(left == 1) {
-    right();
-  } else if(right == 1) {
-    left();
-  } else {
-    stop();
+  /*uint8_t l, m, r;
+  qtrManager(l, m, r);
+
+  // Debug sensor values
+  for (int i = 0; i < sensorCount; i++) {
+    Serial.print(sensorValues[i]);
+    Serial.print('\t');
   }
-}
+  Serial.println();
+
+  if (l == 0 && m == 1 && r == 0) {
+    goStraight();
+  } else if (l == 1) {
+    turnRight();
+  } else if (r == 1) {
+    turnLeft();
+  } else {
+    stopMotor();
+  }
+
+  delay(20);*/
+  moveStart();
+  goStraight();
+  delay(1500);
+  turnRight();
+  delay(1500);
+  turnLeft();
+  delay(1500);
+  stopMotor();
+  delay(1500);
+  }
