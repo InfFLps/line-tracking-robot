4b1b1a7af7cf7eda4c8327b4d5766de4a0a530b9
InfFlps <inflps@proton.me>
2026-03-06 09:16:06 +0800

    Refactor motor functions and update sensor logic
    Refactor motor control functions and sensor handling. Used IR instead of QTR

--- line-tracking-robot.ino
+++ line-tracking-robot.ino
@@ -1,3 +1,9 @@
+/**
+ * @param of sensor1: when 0 then black, if not then white
+ * @param of sensor2: opposite of sensor1
+ *
+ */
+
 #define AIN1 13
 #define AIN2 14
 #define BIN1 32
@@ -8,95 +14,68 @@
 const uint32_t PWM_FREQ = 20000;
 const uint8_t  PWM_RES  = 8;
 
-const int SENSOR_PINS[] = {35, 34, 15, 12, 2, 4};
-const int NUM_SENSOR_PIN = sizeof(SENSOR_PINS) / sizeof(SENSOR_PINS[0]);
+int sensor1 = 23; // Right
+int sensor2 = 27;
 
-int sensorThreshold[NUM_SENSOR_PIN];
-
-void motor(uint8_t leftSpeed, uint8_t rightSpeed) {
-  digitalWrite(AIN1, LOW);
-  digitalWrite(AIN2, HIGH);
-  digitalWrite(BIN1, HIGH);
-  digitalWrite(BIN2, LOW);
+void motor(uint8_t ain1State, uint8_t ain2State, uint8_t bin1State, uint8_t bin2State, uint8_t leftSpeed, uint8_t rightSpeed) {
+  digitalWrite(AIN1, ain1State);
+  digitalWrite(AIN2, ain2State);
+  digitalWrite(BIN1, bin1State);
+  digitalWrite(BIN2, bin2State);
 
   ledcWrite(PWMA, leftSpeed);
   ledcWrite(PWMB, rightSpeed);
 }
 
-void goStraight() { motor(255,255); }
-void turnLeft() { motor(80,255); }
-void turnRight() { motor(255,80); }
-void
-void stopMotor() { motor(0,0); }
-
-void calibrateSensorsWithMovement(int durationMs = 5000, int intervalMs = 50) {
-  int minValues[NUM_SENSOR_PIN];
-  int maxValues[NUM_SENSOR_PIN];
-
-  for (int i = 0; i < NUM_SENSOR_PIN; i++) {
-    minValues[i] = 4095;
-    maxValues[i] = 0;
-  }
-
-  Serial.println("Start calibration: rotate robot over line");
-
-  unsigned long startTime = millis();
-  while (millis() - startTime < durationMs) {
-    motor(150, 5;
-    delay(intervalMs);
-
-    for (int i = 0; i < NUM_SENSOR_PIN; i++) {
-      int val = analogRead(SENSOR_PINS[i]);
-      if (val < minValues[i]) minValues[i] = val;
-      if (val > maxValues[i]) maxValues[i] = val;
-    }
-  }
-
-  stopMotor();
-
-  for (int i = 0; i < NUM_SENSOR_PIN; i++) {
-    sensorThreshold[i] = (minValues[i] + maxValues[i]) / 2;
-    Serial.print("Sensor ");
-    Serial.print(i);
-    Serial.print(" threshold = ");
-    Serial.println(sensorThreshold[i]);
-  }
-
-  Serial.println("Calibration complete!");
+void goStraight(int pwma, int pwmb) { 
+  motor(1, 0, 0, 1, pwma, pwmb); 
 }
-
+void turnLeft(int baseSpeed) {
+  motor(1, 0, 0, 1, baseSpeed / 4, baseSpeed);
+}
+void turnRight(int baseSpeed) {
+  motor(1, 0, 0, 1, baseSpeed, baseSpeed / 4);
+}
+void goBack(int pwma, int pwmb) {
+   motor(0, 1, 1, 0, pwma, pwmb); 
+}
+void stopMotor()  { 
+  motor(0, 0, 0, 0, 0, 0); 
+}
 
 void setup() {
   Serial.begin(115200);
+
   pinMode(AIN1, OUTPUT);
   pinMode(AIN2, OUTPUT);
   pinMode(BIN1, OUTPUT);
   pinMode(BIN2, OUTPUT);
-
   ledcAttach(PWMA, PWM_FREQ, PWM_RES);
   ledcAttach(PWMB, PWM_FREQ, PWM_RES);
-  stopMotor();
 
-  calibrateSensorsWithMovement();
+  pinMode(sensor1, INPUT);
+  pinMode(sensor2, INPUT);
+  stopMotor();
 }
 
 void loop() {
-  motor(100, 100);
-  int SENSOR_VALUE[NUM_SENSOR_PIN];
-  SENSOR_VALUE[0] = analogRead(SENSOR_PINS[0]);
-  SENSOR_VALUE[1] = analogRead(SENSOR_PINS[1]);
-  SENSOR_VALUE[2] = analogRead(SENSOR_PINS[2]);
-  SENSOR_VALUE[3] = analogRead(SENSOR_PINS[3]);
-  SENSOR_VALUE[4] = analogRead(SENSOR_PINS[4]);
-  SENSOR_VALUE[5] = analogRead(SENSOR_PINS[5]);
+  int sval1 = digitalRead(sensor1);
+  int sval2 = digitalRead(sensor2);
 
-  for (int i = 0; i < NUM_SENSOR_PIN; i++) {
-    int detected = SENSOR_VALUE[i] < sensorThreshold[i] ? 1 : 0;
-    Serial.print(SENSOR_VALUE[i]);
-    Serial.print("(");
-    Serial.print(detected);
-    Serial.print(")\t");
+  if(sval1 == 0 && sval2 == 0) {
+    goStraight(200, 200);
+  } else if(sval1 == 1) {
+    turnLeft(200);
+  } else if(sval2 == 1) {
+    turnRight(200);
+  } else {
+    stopMotor();
   }
-  Serial.println();
-  delay(100);
+
+  Serial.println("sensorval: ");
+  Serial.println(sval1);
+  Serial.println("sval2: ");
+  Serial.println(sval2);
+  delay(500);
+
 }
